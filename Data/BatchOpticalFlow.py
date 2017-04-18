5fc820cef4d6a79f15e22d62ec7098c225a733a7
FourSwordKirby <rogliu26@gmail.com>
2017-04-18 19:06:21 -0400

    Got optical flow working

--- Data/BatchOpticalFlow.py
+++ Data/BatchOpticalFlow.py
@@ -1,29 +1,66 @@
 import cv2
 import numpy as np
-
+from PIL import Image
+import pdb
 f1 = cv2.imread('f1.png')
 prvs = cv2.cvtColor(f1,cv2.COLOR_BGR2GRAY)
-print(prvs.shape)
 hsv = np.zeros_like(f1)
 hsv[...,1] = 255
-print(hsv.shape)
 
 f3 = cv2.imread('f3.png')
 nxt = cv2.cvtColor(f3,cv2.COLOR_BGR2GRAY)
 
 flow = cv2.calcOpticalFlowFarneback(prvs,nxt, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
-mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-print(ang.shape)
-print(hsv[...,0].shape)
+def draw_flow(img, flow, step=16):
+    h, w = img.shape[:2]
+    y, x = np.mgrid[step/2:h:step, step/2:w:step].reshape(2,-1).astype(int)
+    fx, fy = flow[y,x].T
+    lines = np.vstack([x, y, x+fx, y+fy]).T.reshape(-1, 2, 2)
+    lines = np.int32(lines + 0.5)
+    vis = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    cv2.polylines(vis, lines, 0, (0, 255, 0))
+    for (x1, y1), (x2, y2) in lines:
+        cv2.circle(vis, (x1, y1), 1, (0, 255, 0), -1)
+    return vis
 
-hsv[...,0] = ang*180/np.pi/2
-hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-rgb = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+def warp_flow(img, flow):
+    h, w = flow.shape[:2]
+    flow = -flow
+    flow[:,:,0] += np.arange(w)
+    flow[:,:,1] += np.arange(h)[:,np.newaxis]
+    res = cv2.remap(img, flow, None, cv2.INTER_LINEAR)
+    return res
 
-print(rgb)
-cv2.imshow('frame2', rgb)
-k = cv2.waitKey()
-if k == ord('s'):
-    cv2.imwrite('opticalfb.png',prev)
-    cv2.imwrite('opticalhsv.png',prev + rgb)
+
+
+cap = cv2.VideoCapture("test.avi")
+
+ret, frame1 = cap.read()
+img = Image.fromarray(np.asarray(cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY), dtype=np.uint8))
+img.save('frame1.png')
+
+ret, frame2 = cap.read()
+img = Image.fromarray(np.asarray(cv2.cvtColor(frame2,cv2.COLOR_BGR2GRAY), dtype=np.uint8))
+img.save('frame2.png')
+
+cap.read()
+cap.read()
+ret, frame3 = cap.read()
+img = Image.fromarray(np.asarray(cv2.cvtColor(frame3,cv2.COLOR_BGR2GRAY), dtype=np.uint8))
+img.save('frame3.png')
+
+prvs = cv2.cvtColor(frame1,cv2.COLOR_BGR2GRAY)
+nxt = cv2.cvtColor(frame3,cv2.COLOR_BGR2GRAY)
+flow = cv2.calcOpticalFlowFarneback(prvs,nxt, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+img = Image.fromarray(np.asarray(warp_flow(nxt, flow), dtype=np.uint8))
+img.save('frameinterp.png')
+
+# cv2.imshow('frame1', frame1)
+# cv2.imshow('frame3', frame3)
+# cv2.imshow('flow', draw_flow(nxt, flow))
+# cv2.imshow('interp', warp_flow(nxt, flow))
+
+cap.release()
+cv2.destroyAllWindows()
+
